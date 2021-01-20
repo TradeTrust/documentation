3099061bcdd67dcce2098922343b8c89bd62cc88
Sim Boon Long <simboonlong@gmail.com>
2021-01-20 15:53:35 +0800

    fix: footer img

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,6 @@ const siteConfig = {
         docs: {
           path: "./docs",
           sidebarPath: require.resolve("./sidebars.json"),
-          editUrl: "https://github.com/TradeTrust/documentation/tree/master",
         },
         theme: {
           customCss: [require.resolve("./src/css/tailwind.css")],
@@ -41,7 +40,7 @@ const siteConfig = {
         {
           items: [
             {
-              html: `<a href="https://www.tradetrust.io" target="_blank" rel="noreferrer noopener"><img src="/static/img/logo/logo-tt-full.svg" alt="TradeTrust Logo" style="max-width: 140px;" /></a>`,
+              html: `<a href="https://www.tradetrust.io" target="_blank" rel="noreferrer noopener"><img src="/img/logo/logo-tt-full.svg" alt="TradeTrust Logo" style="max-width: 140px;" /></a>`,
             },
           ],
         },
