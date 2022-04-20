95a2c9d11cefc657b8ff4e071ea602fa529c9216
Sim Boon Long <simboonlong@gmail.com>
2022-04-20 12:09:36 +0800

    fix: blank test

---
+++ src/theme/DocItem/index.test.tsx
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { render, screen, fireEvent } from "@testing-library/react";
+
+describe("FeedbackWidget", () => {
+  it("wip", () => {
+    expect(true).toBe(true); // wip
+  });
+});
